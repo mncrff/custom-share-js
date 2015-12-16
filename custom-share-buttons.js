90b5cbf8f6debe4cfd4a17de70bac57caddf5221
Alycia Moncrieff <alycia@amoncrieff.com>
2015-12-16 13:57:07 -0800

    able to retrieve share counts, namespaced javascript.

--- custom-share-buttons.js
+++ custom-share-buttons.js
@@ -1,79 +1,122 @@
-function customShareButton(el) {
-	var button = this;
+CustomShare = (function() {
 
-	this.el = el;
-	this.type = getAttr(this.el.getAttribute('data-type'), null);
-	this.href = getAttr(this.el.getAttribute('href'), "");
-	this.count_url = getAttr(this.el.getAttribute('data-count-url'), "");
-	this.count = 0;
-	this.count_el = null;
+	var selection = document.querySelectorAll('.custom-share-button');
+	var buttonsList = [];
 
-	this.el.className += ' ' + this.type + '-custom-share-button';
+	var button = function(el, id) {
+		var self = this;
 
-	//if the button should show a count
-	if (this.count_url) {
-		//add count element to the DOM
-		this.count_el = document.createElement('span');
-		this.count_el.className += ' share-count';
-		this.el.appendChild(this.count_el);
-	}
-	//update count
-	this.updateCount();
+		this.el = el;
+		this.id = id;
+		this.type = getAttr(this.el.getAttribute('data-type'), "");
+		this.href = getAttr(this.el.getAttribute('href'), "");
+		this.share_url = getAttr(this.el.getAttribute('data-share-url'), "");
+		this.show_count = getAttr(this.el.getAttribute('data-count'), "");
 
-	//click event listener
-	this.el.addEventListener('click', onTriggerEl);
+		this.el.className += ' ' + this.type + '-custom-share-button';
+		this.el.setAttribute('id', this.type + "-" + id);
 
-	function onTriggerEl(e) {
-		e.preventDefault();
-		openPopup(button.href, 500, 370);
-	}
-
-	function openPopup(url, width, height) {
-	    var leftPosition, topPosition;
-	    //Allow for borders.
-	    leftPosition = (window.screen.width / 2) - ((width / 2) + 10);
-	    //Allow for title and status bars.
-	    topPosition = (window.screen.height / 2) - ((height / 2) + 50);
-	    //Open the window.
-	    window.open(url, "Share Window", "status=no,height=" + height + ",width=" + width + ",resizable=yes,left=" + leftPosition + ",top=" + topPosition + ",screenX=" + leftPosition + ",screenY=" + topPosition + ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no");
-	}
-
-	function getAttr(attr, defaultValue) {
-		return (typeof attr == 'undefined' || attr == null) ? defaultValue : attr;
-	}
-}
-
-customShareButton.prototype.updateCount = function() {
-	//if the button should show a count
-	if (this.count_url) {
-		var endpoint = '';
-
-		switch(this.type) {
-			case 'facebook':
-				endpoint = "http://graph.facebook.com/?id=http://www.microsoft.com";
-				break;
-			case 'twitter':
-				endpoint = "http://urls.api.twitter.com/1/urls/count.json?url=http://www.microsoft.com";
-				break;
-			case 'linkedin':
-				endpoint = "http://www.linkedin.com/countserv/count/share?url=http://www.microsoft.com";
-				break;
+		//if the button should show a count
+		if (this.show_count) {
+			/* get count by adding script to body 
+			   that returns JSON with count stats 
+			   via a callback */
+			this.addScript();
 		}
-		
-		//set count as text inside button
-		this.count_el.textContent = this.count;
+
+		//click event listener
+		this.el.addEventListener('click', onTriggerEl);
+
+		function onTriggerEl(e) {
+			e.preventDefault();
+			openPopup(self.href, 500, 370);
+		}
+
+		function openPopup(url, width, height) {
+		    var leftPosition, topPosition;
+		    //Allow for borders.
+		    leftPosition = (window.screen.width / 2) - ((width / 2) + 10);
+		    //Allow for title and status bars.
+		    topPosition = (window.screen.height / 2) - ((height / 2) + 50);
+		    //Open the window.
+		    window.open(url, "Share Window", "status=no,height=" + height + ",width=" + width + ",resizable=yes,left=" + leftPosition + ",top=" + topPosition + ",screenX=" + leftPosition + ",screenY=" + topPosition + ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no");
+		}
+
+		function getAttr(attr, defaultValue) {
+			return (typeof attr == 'undefined' || attr == null) ? defaultValue : attr;
+		}
+	};
+
+	button.prototype = {
+		addScript: function() {
+			//if the button should show a count
+			if (this.type && this.type != 'twitter') {
+				var endpoint = "";
+				this.script = document.createElement('script');
+
+				switch(this.type) {
+					case 'facebook':
+						endpoint = "https://graph.facebook.com/?id=http://www.microsoft.com";
+						this.script.src = endpoint+"&callback=CustomShare.callbackFacebook";
+						break;
+					case 'linkedin':
+						endpoint = "https://www.linkedin.com/countserv/count/share?url=http://www.microsoft.com";
+						this.script.src = endpoint+"&callback=CustomShare.callbackLinkedin";
+						break;
+				}
+
+				this.el.parentNode.insertBefore(this.script, this.el.nextSibling);
+			}
+		}
 	}
-}
 
-function testCallback(result) {
-	console.log(result);
-	result.shares && console.log('The count is: ', result.shares);
-}
+	var callbackFacebook = function(json){
+		var btn = getCurrentScript().previousSibling;
+		//add count element to the DOM
+		addCount(btn, json.shares);
+	}
+	var callbackLinkedin = function(json){
+		var btn = getCurrentScript().previousSibling;
+		//add count element to the DOM
+		addCount(btn, json.count);
+	}
 
-window.onload = function(){
-	var sharebtns = document.querySelectorAll('.custom-share-button');
+	var getCurrentScript = function() {
+		return document.currentScript || (function() {
+	      var scripts = document.getElementsByTagName('script');
+	      return scripts[scripts.length - 1];
+	    })();
+	}
 
-	window.Share = Array.prototype.map.call(sharebtns, function(el, i){
-		return new customShareButton(el);
+	var addCount = function(btn, val) {
+		var count_el = document.createElement('span');
+		count_el.className = 'share-count';
+		count_el.setAttribute('data-val', val);
+		count_el.innerHTML = '<span style="margin: -1px; padding: 0; width: 1px; height: 1px; overflow: hidden; clip: rect(0 0 0 0); clip: rect(0, 0, 0, 0); position: absolute;">Number of Shares:</span> '+formatCount(val);
+		btn.appendChild(count_el);
+	}
+
+	var formatCount = function(val) {
+		if (val > 9999) {
+			return Math.round(val/1000)+"k";
+		} else if (val > 999) {
+			return (Math.round(val/100) / 10)+"k";
+		} else {
+			return val;
+		}
+	}
+
+	// window.onload = function(){
+	buttonsList = Array.prototype.map.call(selection, function(el, i){
+		return new button(el, i);
 	});
-}
+	//};
+
+	// more than one object
+	return {
+		buttonsList: buttonsList,
+		callbackFacebook: callbackFacebook,
+		callbackLinkedin: callbackLinkedin
+	};
+
+})();
